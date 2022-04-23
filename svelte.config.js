5b5163446beb823141cf2f2df909a0ed5971ae54
Kris <kriistjanm@gmail.com>
2022-04-23 19:49:20 +0700

    Disable edge deploy

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,12 @@
 import preprocess from 'svelte-preprocess';
-// import adapter from '@sveltejs/adapter-auto';
-import adapter from '@sveltejs/adapter-vercel'
+import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-vercel'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		// adapter: adapter(),
-		adapter: adapter({ edge: true }),
+		adapter: adapter(),
+		// adapter: adapter({ edge: true }),
 		trailingSlash: 'always'
 	},
 
